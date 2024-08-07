5d208bf4ab9fe9e5d403783362d353c426d0b5e9
d-yokoi-prg <15cu0132.1120@gmail.com>
2024-08-07 23:43:54 +0900

    Update desktop.java

--- desktopApp/desktop.java
+++ desktopApp/desktop.java
@@ -1,34 +1,49 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.util.*;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.JPanel;
 
+//デスクトップアプリクラス
+class desktopApp extends JFrame{
+	
+	desktopApp(String title){
+		super(title);
+		
+		//ウィンドウのサイズを設定
+		setSize(500, 500);
+        //ウィンドウをデスクトップ上に表示する
+        setVisible(true);
 
-public class desktop {
-    public static void main(String args[]){
-
-        Scanner sc = new Scanner(System.in);
-
-        //コンストラクタの引数でアプリタイトルを設定
-        JFrame jf = new JFrame("Hello World");
-
-
-        //デスクトップに表示する画面のサイズ（幅、高さ）
-        jf.setSize(500, 500);
-
+        //「×」ボタンで画面を閉じたらアプリケーションを終了する
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+    
+	//表示するラベルを設定
+	public void setLabel() {
         //画面上に表示するメッセージ
         JLabel jl = new JLabel("Hello");
         jl.setFont(new Font("MS 明朝", Font.PLAIN, 32));
 
         //メッセージを画面上部に表示させる
-        jl.setVerticalAlignment(JLabel.TOP);
-        jf.add(jl);
+        jl.setVerticalAlignment(JLabel.CENTER);
+        jl.setPreferredSize(new Dimension(100, 50));
+        
+        JPanel p = new JPanel();
+        p.add(jl);
+		
+	    Container contentPane = getContentPane();
+	    contentPane.add(p, BorderLayout.CENTER);
+	}
+}
+public class desktop {
+    public static void main(String args[]){
 
-        //ウィンドウをデスクトップ上に表示する
-        jf.setVisible(true);
-
-        //「×」ボタンで画面を閉じたらアプリケーションを終了する
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	//コンストラクタの引数でアプリタイトルを設定
+    	desktopApp jf = new desktopApp("title");
+    	jf.setLabel();
     }
 }
